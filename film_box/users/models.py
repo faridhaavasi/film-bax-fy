a325a1a583145e3c63da18f116a76c2690778606
faridhaavasi <faridhavasi777@gmail.com>
2024-07-27 18:10:52 +0330

    costum user

--- film_box/users/models.py
+++ film_box/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from film_box.common.models import BaseModel
-
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager as BUM
 from django.contrib.auth.models import PermissionsMixin
@@ -8,11 +7,13 @@ from django.contrib.auth.models import PermissionsMixin
 
 
 class BaseUserManager(BUM):
-    def create_user(self, email, is_active=True, is_admin=False, password=None):
+    def create_user(self, email, phone_number, is_active=True, is_admin=False, password=None):
         if not email:
             raise ValueError("Users must have an email address")
+        if not phone_number:
+            raise ValueError("You must enter the phone number")
 
-        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
+        user = self.model(email=self.normalize_email(email.lower()), phone_number=phone_number, is_active=is_active, is_admin=is_admin)
 
         if password is not None:
             user.set_password(password)
@@ -24,9 +25,10 @@ class BaseUserManager(BUM):
 
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email, phone_number, password=None):
         user = self.create_user(
             email=email,
+            phone_number=phone_number,
             is_active=True,
             is_admin=True,
             password=password,
@@ -41,14 +43,17 @@ class BaseUserManager(BUM):
 class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(verbose_name = "email address",
-                              unique=True)
+                              unique=True, db_index=True)
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name="phone_number")
+
 
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = BaseUserManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ("email", )
 
     def __str__(self):
         return self.email
@@ -56,16 +61,6 @@ class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
     def is_staff(self):
         return self.is_admin
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
-    posts_count = models.PositiveIntegerField(default=0)
-    subscriber_count = models.PositiveIntegerField(default=0)
-    subscription_count = models.PositiveIntegerField(default=0)
-    bio = models.CharField(max_length=1000, null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.user} >> {self.bio}"
 
 
 
