47c70c8ed497a596a61d06c85a952da90a6e2cae
faridhaavasi <faridhavasi777@gmail.com>
2024-08-04 18:36:48 +0330

    finall and edit

--- config/django/base.py
+++ config/django/base.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # custom
+    'film_box.logger.middlewares.LoggerMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
