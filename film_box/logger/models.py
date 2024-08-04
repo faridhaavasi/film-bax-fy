350077fc0516a45760a26dbfd8a8531678b7778e
faridhaavasi <faridhavasi777@gmail.com>
2024-08-04 17:23:51 +0330

    add model logger

--- film_box/logger/models.py
+++ film_box/logger/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from film_box.common.models import Basemodel
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Logger(Basemodel):
+    url = models.URLField(max_length=512)
+    request = models.JSONField(null=True, blank=True)
+    response = models.JSONField(null=True, blank=True)
+    method = models.CharField(max_length=32)
+    status = models.PositiveIntegerField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='logger', null=True, blank=True)
+    
