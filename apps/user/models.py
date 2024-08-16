1029caacf45e5850c9191bbe1015904ca9329ded
faridhaavasi <faridhavasi777@gmail.com>
2024-08-16 19:56:24 +0330

    '123'

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+from apps.common.models import BaseModel
+
+
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where phone_number is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, phone_number, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not phone_number:
+            raise ValueError(_("The phone_number must be set"))
+        phone_number = phone_number
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given phone_number and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+
+
+
+class CustomUser(AbstractBaseUser, BaseModel):
+    phone_number = models.CharField(max_length=11, unique=True, db_index=True)
+    fullname = models.CharField(null=True, blank=True)
+    id_name = models.CharField(unique=True, db_index=True, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.phone_number
+
+
+
+
+
+
