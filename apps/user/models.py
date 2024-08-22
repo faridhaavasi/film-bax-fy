790d723fb100a2be3c0e5465fcbebce7721fb366
faridhaavasi <faridhavasi777@gmail.com>
2024-08-22 19:12:47 +0330

    edit and dibug user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 from apps.common.models import BaseModel
 
@@ -29,13 +29,12 @@ class CustomUserManager(BaseUserManager):
         Create and save a SuperUser with the given phone_number and password.
         """
         extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
+
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+
         return self.create_user(phone_number, password, **extra_fields)
 
 
@@ -48,15 +47,31 @@ class CustomUser(AbstractBaseUser, BaseModel):
     id_name = models.CharField(unique=True, db_index=True, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
+
     def __str__(self):
         return self.phone_number
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
 
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
 
 
