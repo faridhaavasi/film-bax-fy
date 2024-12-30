9bae91a38478e9323795afd596bb69f1ece0f15c
faridhaavasi <faridhavasi777@gmail.com>
2024-12-30 18:12:24 +0330

    remove deprecated files and configurations

--- apps/user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from apps.user.models import CustomUser
-
-# Register your models here.
-
-@admin.register(CustomUser)
-class ModelNameAdmin(admin.ModelAdmin):
-    ...
